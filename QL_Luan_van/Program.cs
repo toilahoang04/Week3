a58619b39cefbffe5151cf7a377ab2574fc057cf
toilahoang04 <levanviethoang04@gmail.com>
2024-02-21 09:50:26 +0700

    Hoang-First

--- QL_Luan_van/Program.cs
+++ QL_Luan_van/Program.cs
@@ -16,7 +16,7 @@ namespace QL_Luan_van
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FManage());
         }
     }
 }
